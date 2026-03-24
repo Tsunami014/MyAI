659e8cc756318a5913d1771d6199559cd4fc931e
Tsunami014 <tsunami014@duck.com>
2026-03-24 17:38:50 +1100

    More parsing, and added Qualities!

--- parse.py
+++ parse.py
@@ -1,50 +1,126 @@
 import spacy
 import spacy.cli
 
+class QualityTypes:
+    VALUE = 'V'
+    MODIFIER = 'M'
+
+class Quality:
+    __slots__ = ['txt', 'typ', 'xtra']
+    def __init__(self, txt, typ, xtra=None):
+        self.txt = txt
+        self.typ = typ
+        self.xtra = list(xtra or [])
+    def __eq__(self, oth):
+        if isinstance(oth, Quality):
+            return oth.txt == self.txt
+        return oth == self.txt
+    def __str__(self):
+        if self.xtra:
+            xtra = "" if not self.xtra else str(self.xtra)
+            return f"{self.typ}<{self.txt} {xtra}>"
+        return f"{self.typ}({self.txt})"
+    def __repr__(self):
+        return str(self)
+
 class Tok:
-    __slots__ = ['tok', 'children', 'info']
+    __slots__ = ['tok', 'children', 'info', 'personalInfo']
     def __init__(self, token):
         self.tok = token
         self.children = []
-        self.info = self.thisApplication()
+        self.info = []
         for c in token.children:
             t = Tok(c)
-            appl, keep = t.application()
+            appl, keep = t.application(self.tok)
             if keep:
                 self.children.append(t)
             self.info.extend(appl)
+        self.thisApplication()
 
-    def application(self):
+    def _apply_map(self, morph, mapping, appls):
+        for k, v in morph.items():
+            for nam in mapping:
+                if k == nam:
+                    vals = mapping[nam]
+                    if v in vals:
+                        appls.append(Quality(vals[v], QualityTypes.VALUE))
+                        break
+
+    def application(self, ontotok):
         if self.tok.pos_ == "PUNC" or "punc" in self.tok.dep_:
             return [], True
         appls = []
         keep = True
-        if self.tok.pos_ == "PART":
+        morph = self.tok.morph.to_dict()
+
+        self._apply_map(morph, {
+            "Degree": {
+                "Abs": "best",
+                "Pos": "positive",
+                "Equ": "same",
+                "Cmp": "compared",
+                "Sup": "most",
+                # In size, force, affection, etc.
+                "Dim": "small",
+                "Aug": "large",
+            },
+            "Poss": {
+                "Yes": "owned",
+            },
+            "Polarity": {
+                "Neg": "no",
+                "Pos": "yes",
+            },
+            "Definite": {
+                "Ind": "any",
+                "Spec": "specific",
+                "Def": "the",
+            },
+        }, appls)
+        if self.tok.pos_ in {"PART", "DET"} or self.tok.dep_ in {"det"}:
             keep = False
-            morph = self.tok.morph.to_dict()
-            for k, v in morph.items():
-                if k == "Polarity":
-                    pols = {"Neg": 'no', "Pos": 'yes'}
-                    if v in pols:
-                        appls.append(pols[v])
-        return appls, keep
+
+        if ontotok.pos_ in {"NOUN", "PRON", "ADJ", "ADV"} and self.tok.pos_ in {"PRON", "ADJ", "ADV"}:
+            keep = False
+            done = False
+            if self.tok.pos_ == "PRON" and "PronType" in morph:
+                vals = {
+                    "Prs": "I",
+                    "Rcp": "we",
+                    "Art": "a",
+                    "Int": "?",
+                    "Tot": "everyone",
+                    "Neg": "noone",
+                    "Ind": "some"
+                }
+                typ = morph["PronType"]
+                if typ in vals:
+                    appls.append(Quality(vals[typ], QualityTypes.VALUE, self.personalInfo))
+                    done = True
+            if not done:
+                appls.append(Quality(self.tok.text, QualityTypes.MODIFIER, self.personalInfo))
+
+        if keep:
+            return appls, True
+        return self.info+appls, False
 
     def thisApplication(self):
-        appls = []
+        self.personalInfo = []
         morph = self.tok.morph.to_dict()
-        for k, v in morph.items():
-            if k == "Number":
-                if v == 'Sing':
-                    appls.append("One")
-                elif v == 'Dual':
-                    appls.append("Two")
-                elif v == 'Tri':
-                    appls.append("Three")
-                elif v == 'Plur':
-                    appls.append("Several")
-                elif v == 'Pauc':
-                    appls.append("A few")
-        return appls
+        self._apply_map(morph, {
+            "Number": {
+                "Sing": "one",
+                "Dual": "two",
+                "Tri": "three",
+                "Plur": "four",
+                "Pauc": "a few",
+            },
+            "Person": {
+                "1": "1stPerson",
+                "2": "2ndPerson",
+                "3": "3rdPerson",
+            },
+        }, self.personalInfo)
 
     def __eq__(self, oth):
         return oth.tok == self.tok
@@ -56,8 +132,9 @@ class Tok:
 
     def __str__(self):
         base = "" if self.tok.lemma_ == self.tok.text else f" ({self.tok.lemma_})"
-        xtra = "" if not self.tok.morph else " - "+", ".join(self.info)
-        return f"{self.tok.text}{base} {self.tok.pos_}, {self.tok.dep_}"+xtra
+        xtra1 = "" if not self.personalInfo else " - "+", ".join(str(i) for i in self.personalInfo)
+        xtra2 = "" if not self.info else " - "+", ".join(str(i) for i in self.info)
+        return f"{self.tok.text}{base} {self.tok.pos_}, {self.tok.dep_}"+xtra1+xtra2
     def __repr__(self):
         return self.tok.text
 
@@ -106,6 +183,21 @@ class Parser:
     def is_root(self, token):
         return token.pos_ in {"VERB", "AUX"} and \
             token.dep_ in {"ROOT", "conj", "advcl"}
+
+    def debug_tree(self, token, level=0):
+        base = "" if token.lemma_ == token.text else f" ({token.lemma_})"
+        xtra = "" if not token.morph else " - "+str(token.morph)
+        txt = f"{token.text}{base} {token.pos_}, {token.dep_}"+xtra
+
+        outs = ["  " * level + txt]
+        for child in token.children:
+            outs.append(self.debug_tree(child, level + 1))
+        return '\n'.join(outs)
+
+    def dbug(self, txt):
+        doc = self.doc(txt)
+        roots = [t for t in doc if self.is_root(t)]
+        return '\n'.join(self.debug_tree(r) for r in roots)
 
     def __call__(self, txt):
         doc = self.doc(txt)
